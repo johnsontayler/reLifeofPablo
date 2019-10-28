1a241a2bda40657aae77671a3e4f929ffddd31d5
Tayler Johnson <johnsontayler@me.com>
2019-10-28 10:40:34 -0300

    moved audience and kanye loader into new js file

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 import * as THREE from '/node_modules/three/build/three.module.js';
 import { OrbitControls } from '/node_modules/three/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from '/node_modules/three/examples/jsm/loaders/GLTFLoader.js';
-import { SkeletonUtils } from '/node_modules/three/examples/jsm/utils/SkeletonUtils.js';
-import { FBXLoader } from '/node_modules/three/examples/jsm/loaders/FBXLoader.js';
+import { load_audience_kanye } from '/js/load_audience_kanye.js';
 
+///////////// Page Loading /////////
 var loadingPage = function () {
   var loading = document.getElementById( 'loading' );
 
@@ -14,7 +13,9 @@ var loadingPage = function () {
   }, 10000);
 };
 loadingPage();
+/////////////////////////////////////
 
+/// Landing Page Button Animation ///
 var changeButtonText = function () {
   var text = ["Click to Relive", "Click to Rejoice", "Click to Reincarnate", "Click to Recreate", "Click to Resurrect"];
 
@@ -32,34 +33,19 @@ var changeButtonText = function () {
   }, 1000);
 };
 changeButtonText();
+/////////////////////////////////////
 
-var kanyeLoader = new FBXLoader();
-kanyeLoader.load( 'models/liam.fbx', function ( object ) {
-    // mixer = new THREE.AnimationMixer( object );
-    // var action = mixer.clipAction( object.animations[ 0 ] );
-    // action.play();
-    object.castShadow = true;
-    object.receiveShadow = true;
-    object.scale.set( 0.08, 0.08, 0.08 )
-    object.position.y = 3.3;
-    stage.add( object );
-} );
+////////// Global Variables ////////
+var camera = null;
+var listener = null;
+var worldScene = null;
+var stage = null;
+////////////////////////////////////
 
-var array30 = new Array(30);
+/////////// Build World ////////////
+load_audience_kanye();
 
-var MODELS = [ { name: "audience" } ];
 
-var UNITS = [
-  {
-    modelName: "audience", // Will use the 3D model from file models/audience.gltf
-    position: { x: 0, y: -20, z: 0 }, // Where to put the unit in the scene
-    scale: 0.2 // Scaling of the unit. 1.0 means: use original size, 0.1 means "10 times smaller", etc.
-  }
-];
-
-var numLoadedModels = 0;
-createUnits();
-loadModels();
 
 var camera = new THREE.PerspectiveCamera( 50, window.innerWidth/window.innerHeight, 0.1, 1000 );
 camera.position.set( 120, 80, 15 );
@@ -67,7 +53,7 @@ camera.position.set( 120, 80, 15 );
 var listener = new THREE.AudioListener();
 camera.add( listener );
 
-var scene = new THREE.Scene();
+var worldScene = new THREE.Scene();
 
 
 // STAGE // 
@@ -85,15 +71,8 @@ var material = new THREE.MeshFaceMaterial( cubeMaterials );
 var stage = new THREE.Mesh( geometry, material );
 stage.position.set( -80, 15, -100 );
 stage.receiveShadow = true;
-scene.add( stage );
+worldScene.add( stage );
 // STAGE //
-
-// YEEZY FANS // 
-// var audience1 = createAudienceGroup( ( 0, -20, 0 ) );
-// //var audience2 = createAudienceGroup( ( 0, -10, 0 ) )
-// console.log( audience1 );
-// scene.add( audience1 );
-// YEEZY FANS // 
 
 // GENERAL ADMISSION FLOOR // 
 var gaFloor = new THREE.Mesh(
@@ -102,20 +81,12 @@ var gaFloor = new THREE.Mesh(
 )
 gaFloor.position.set( 0, -20, 0 );
 gaFloor.rotation.x = Math.PI / 2;
-scene.add(gaFloor);
+worldScene.add(gaFloor);
 // GENERAL ADMISSION FLOOR // 
 
 // LIGHTING // 
 var directionalLight = new THREE.DirectionalLight( 0x655e66 );
-scene.add( directionalLight );
-
-// var kanyeSpotLight = new THREE.SpotLight( 0xa71919 );
-// kanyeSpotLight.position.set( 10, 140, 40);
-// kanyeSpotLight.angle = 0.2;
-// scene.add( kanyeSpotLight );
-
-// var kanyeSpotLightHelper = new THREE.SpotLightHelper( kanyeSpotLight );
-// scene.add( kanyeSpotLightHelper );
+worldScene.add( directionalLight );
 
 var stageLight = new THREE.SpotLight( 0xf98125, 2 );
 stageLight.position.set( 0, 150, 0 );
@@ -124,11 +95,11 @@ stageLight.penumbra = 0.6;
 
 stageLight.target.position.set(stage.position.x, -150, stage.position.y );
 
-scene.add( stageLight );
-scene.add( stageLight.target );
+worldScene.add( stageLight );
+worldScene.add( stageLight.target );
 
 // var spotLightHelper1 = new THREE.SpotLightHelper( stageLight );
-// scene.add( spotLightHelper1 );
+// worldScene.add( spotLightHelper1 );
 // LIGHTING // 
 
 // MUSIC // 
@@ -190,7 +161,7 @@ if (sound) {
   };
 };
 
-scene.add( sound );
+worldScene.add( sound );
 
 // Menu Song Selection // 
 if (sound) {
@@ -276,7 +247,7 @@ window.addEventListener( 'resize', function ( )
 
 // ANIMATION //
 var animate = function () {
-  renderer.render( scene, camera );
+  renderer.render( worldScene, camera );
 
   var targetPosition = 1;
   var originalPositionX = -80;
@@ -298,160 +269,9 @@ var animate = function () {
 };
 // ANIMATION //
 
-function createUnits() {
-  create30UnitsPositive();
-  create30UnitsNegative();
-  create30UnitsXNegative();
-  create30UnitsYNegative();
-} 
+///////////////// START MODEL LOADING FUNCTIONS ///////////////////////
 
-function create30UnitsPositive() {
-  Array.from(array30).forEach(function() {
-    var createdUnit = {
-      modelName: "audience", // Will use the 3D model from file models/audience.gltf
-      position: randomUnitPositionPositive(), // Where to put the unit in the scene
-      scale: 0.2 // Scaling of the unit. 1.0 means: use original size, 0.1 means "10 times smaller", etc.
-    }
-    UNITS.push(createdUnit);
-  });
-}
-
-function create30UnitsNegative() {
-  Array.from(array30).forEach(function() {
-    var createdUnit = {
-      modelName: "audience", // Will use the 3D model from file models/audience.gltf
-      position: randomUnitPositionNegative(), // Where to put the unit in the scene
-      scale: 0.2 // Scaling of the unit. 1.0 means: use original size, 0.1 means "10 times smaller", etc.
-    }
-    UNITS.push(createdUnit);
-  });
-}
-
-function create30UnitsXNegative() {
-  Array.from(array30).forEach(function() {
-    var createdUnit = {
-      modelName: "audience", // Will use the 3D model from file models/audience.gltf
-      position: randomUnitPositionXNeg(), // Where to put the unit in the scene
-      scale: 0.2 // Scaling of the unit. 1.0 means: use original size, 0.1 means "10 times smaller", etc.
-    }
-    UNITS.push(createdUnit);
-  });
-}
-
-function create30UnitsYNegative() {
-  Array.from(array30).forEach(function() {
-    var createdUnit = {
-      modelName: "audience", // Will use the 3D model from file models/audience.gltf
-      position: randomUnitPositionYNeg(), // Where to put the unit in the scene
-      scale: 0.2 // Scaling of the unit. 1.0 means: use original size, 0.1 means "10 times smaller", etc.
-    }
-    UNITS.push(createdUnit);
-  });
-}
-
-function randomUnitPositionPositive() {
-  var position = {
-    x: ( Math.random() * 200 ),
-    y: -20,
-    z: ( Math.random() * 200 )
-  };
-  return position;
-}
-
-function randomUnitPositionNegative() {
-  var position = {
-    x: -( Math.random() * 200 ),
-    y: -20,
-    z: -( Math.random() * 200 )
-  };
-  return position;
-}
-
-function randomUnitPositionXNeg() {
-  var position = {
-    x: -( Math.random() * 200 ),
-    y: -20,
-    z: ( Math.random() * 200 )
-  };
-  return position;
-}
-
-function randomUnitPositionYNeg() {
-  var position = {
-    x: ( Math.random() * 200 ),
-    y: -20,
-    z: -( Math.random() * 200 )
-  };
-  return position;
-}
-
-function loadModels() {
-  for ( var i = 0; i < MODELS.length; ++ i ) {
-    var m = MODELS[ i ];
-    loadGltfModel( m, function () {
-      ++ numLoadedModels;
-      if ( numLoadedModels === MODELS.length ) {
-        console.log( "All models loaded, time to instantiate units..." );
-        instantiateUnits();
-      }
-    } );
-  }
-}
-
-/* Look at UNITS configuration, clone necessary 3D model scenes */
-function instantiateUnits() {
-  var numSuccess = 0;
-  for ( var i = 0; i < UNITS.length; ++ i ) {
-    var u = UNITS[ i ];
-    var model = getModelByName( u.modelName );
-    if ( model ) {
-      // var skeletonUtils = new SkeletonUtils();
-      var clonedScene = SkeletonUtils.clone( model.scene );
-      if ( clonedScene ) {
-        // Different models can have different configurations of armatures and meshes. Therefore,
-        // We can't set position, scale or rotation to individual mesh objects. Instead we set
-        // it to the whole cloned scene and then add the whole scene to the game world
-        // Note: this may have weird effects if you have lights or other items in the GLTF file's scene!
-        numSuccess ++;
-        scene.add( clonedScene );
-        if ( u.position ) {
-          clonedScene.position.set( u.position.x, u.position.y, u.position.z );
-        }
-        if ( u.scale ) {
-          clonedScene.scale.set( u.scale, u.scale, u.scale );
-        }
-            }
-    } else {
-      console.error( "Can not find model", u.modelName );
-    }
-  }
-  console.log( `Successfully instantiated ${numSuccess} units` );
-}
-
-function getModelByName( name ) {
-  for ( var i = 0; i < MODELS.length; ++ i ) {
-    if ( MODELS[ i ].name === name ) {
-      return MODELS[ i ];
-    }
-  }
-  return null;
-}
-
-/**
- * Load a 3D model from a GLTF file. Use the GLTFLoader.
- * param model {object} Model config, one item from the MODELS array. It will be updated inside the function!
- * param onLoaded {function} A callback function that will be called when the model is loaded
- */
-function loadGltfModel( model, onLoaded ) {
-  var loader = new GLTFLoader();
-  var modelName = "models/" + model.name + ".gltf";
-  loader.load( modelName, function ( gltf ) {
-    var scene = gltf.scene;
-    model.scene = scene;
-    console.log( "Done loading model", model.name );
-    onLoaded();
-  } );
-}
+/////////////////////////////////////////////////////////
 
 var startButton = document.getElementById( 'startButton' );
 startButton.addEventListener( 'click', function () {
